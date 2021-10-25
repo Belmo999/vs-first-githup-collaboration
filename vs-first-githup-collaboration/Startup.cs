1ef7c2b4b6e469c92e0f67da6ea6818039b6acc4
Belwaz999 <Belwaz999@gmail.com>
2021-10-25 10:57:09 +0100

    I changed it to H2

--- vs-first-githup-collaboration/Startup.cs
+++ vs-first-githup-collaboration/Startup.cs
@@ -32,7 +32,7 @@ namespace vs_first_githup_collaboration
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("<h1>Hello World!</h1>");
+                    await context.Response.WriteAsync("<h2>Hello World!</h2>");
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
