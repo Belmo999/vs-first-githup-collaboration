a9bfb0c6a502c0992e7316da6350d8f02aaeb4e7
Belwaz999 <Belwaz999@gmail.com>
2021-10-25 10:35:20 +0100

    change hello text and make it bolder

--- vs-first-githup-collaboration/Startup.cs
+++ vs-first-githup-collaboration/Startup.cs
@@ -32,6 +32,7 @@ namespace vs_first_githup_collaboration
             {
                 endpoints.MapGet("/", async context =>
                 {
+                    await context.Response.WriteAsync("<h1>Hello World!</h1>");
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
